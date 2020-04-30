b0a8de5c34bf5455decd662d1a2312bbc6ba3aec
Sreelesh-nambiar <64524277+Sreelesh-nambiar@users.noreply.github.com>
2020-04-30 17:59:53 +0530

    modules added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { StarComponent } from './star.component';
 import { FormsModule } from '@angular/forms';
 
+
+
 @NgModule({
   declarations: [
     StarComponent
-  ],
-  imports: [
-    CommonModule
   ],
   exports: [
     StarComponent,
     CommonModule,
     FormsModule
+  ],
+  imports:[
+    CommonModule
   ]
 })
 export class SharedModule { }
